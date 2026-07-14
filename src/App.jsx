fc8b312affe734f04e21bfe6d6ec47e12ad84316
Dr.ME <vacheslavtkachik@gmail.com>
2026-07-14 22:30:15 +0300

    feat: added vercel tools

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { SmoothScrolling } from '@/components/layout/SmoothScrolling/SmoothScrolling';
 import LoadingScreen from '@/components/ui/LoadingScreen/LoadingScreen';
 import '@/styles/App.scss';
@@ -10,22 +12,27 @@ const ProjectPage = lazy(() => import('@/pages/ProjectPage/ProjectPage'));
 export default function App() {
   return (
     <Suspense fallback={<LoadingScreen />}>
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <SmoothScrolling>
-            <Main />
-          </SmoothScrolling>
-        }
-      />
+      <Analytics /> // Vercel Analytics
+      <SpeedInsights /> // Vercel Speed Insights
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <SmoothScrolling>
+              <Main />
+            </SmoothScrolling>
+          }
+        />
 
-      <Route path="/project/:id" element={
-        <SmoothScrolling>
-          <ProjectPage />
-        </SmoothScrolling>
-      } />
-    </Routes>
+        <Route
+          path="/project/:id"
+          element={
+            <SmoothScrolling>
+              <ProjectPage />
+            </SmoothScrolling>
+          }
+        />
+      </Routes>
     </Suspense>
   );
 }
